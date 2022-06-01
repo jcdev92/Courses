9647e822be45f4b3329adc9a8a8dab1d812fa19a
jcdev92 <jcdeveloper92@gmail.com>
2022-06-01 14:03:18 -0400

    academlo bootcamp fundamentos

---
+++ academlo/fundamentos/variables_js/01_variables.js
@@ -0,0 +1,29 @@
+/**********************************/
+/* VARIABLES ==> VAR, LET, CONST */
+/*********************************/
+/*
+¿Ques es una variable?
+Es un espacio en memoria donde se almacena una referencia a un valor.
+*/
+
+/* Reglas de nomenclatura de variables:
+- Usar solo let, var o const para declarar variables.
+- No puede contener palabras reservadas de JavaScript.
+- No puede comenzar con un número.
+- No puede contener espacios.
+- Solo pueden contener letras, números y guiones bajos y el signo de $.
+para el nombre de la variable que contiene varias palabras se suele usar camelCase. Es decir, palabras van una detrás de la otra, con cada palabra iniciando con la primera letra en mayúscula, ejemplo: miPrimeraVariable */
+
+/**********************/
+/* VARIABLES ==> VAR */
+/*********************/
+
+/**********************/
+/* VARIABLES ==> LET */
+/*********************/
+
+/************************/
+/* VARIABLES ==> CONST */
+/***********************/
+
+// Más Info: https://www.notion.so/academlo/Variables-fc11a03ee70042c39884b3ff5ce2a34f
