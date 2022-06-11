42c7cb819efeadc796fdaa27f76b93321399bfdc
jcdev92 <jcdeveloper92@gmail.com>
2022-06-10 20:09:33 -0400

    training

--- academlo/fundamentos/TEMAS/Bucles/ejerciciosConBucles.js
+++ academlo/fundamentos/TEMAS/Bucles/ejerciciosConBucles.js
@@ -3,20 +3,49 @@
 /**************************/
 
 // 1. Crea un arreglo con los números del 1 al 400
-numeros = 0
-while (numeros < 401) {
-  numeros++
-}
-console.log(numeros)
+// numeros = 0
+// arrayNumbers = []
+// while (numeros < 400) {
+//   numeros++
+//   arrayNumbers.push(numeros)
+// }
+// console.log(arrayNumbers)
 
 // 2. Crea una función que reciba como parámetros 2 números, y retorne un arreglo con todos los valores en el rango de esos 2 números (incluyéndolos), por ejemplo, si recibe 1 y 4 la función debe de retornar el arreglo [1, 2, 3, 4], asumiremos que el segundo parámetro recibido es mayor al primero.
 
+// function numbersArr(number1, number2) {
+//   let arrNums = [];
+//   for (number1; number2 >= number1; number1++) {
+//       arrNums.push(number1)
+//   }
+//   return arrNums
+// }
+
+// console.log(numbersArr(12,30))
+
 // 3. Crea una función que lea un array de N cantidad de números y retorne la suma de todos esos números.
+// function nsum(arr) {
+//     sum = 0;
+//     for (let i = 0; i <= arr.length-1; i++) {
+//       sum += arr[i];
+//     }
+//     return sum
+// }
+// console.log(nsum([5,8,12,20]));
+// console.log(nsum(arrayNumbers));
 
 // 4. Crea una función que reciba como parámetro un array de N cantidad de elementos y retorne el elemento que se encuentra en la mitad
 //    - En caso de que el número de la mitad sea uno solo el programa debe devolverlo, por ejemplo, del arreglo [1, 3, 4] devolvemos el 3
 //    - En caso de que hayan 2 números en medio devolveremos un array de números, por ejemplo, en el array [1, 3, 4, 5, 5, 3] devolveremos [4, 5]
-
+function middlearr(arreglo) {
+  mid = 0
+  for(var i = 0; i < arreglo.length; i++){
+    mid += i;
+  }
+  middle = mid/arreglo.length
+  return middle;
+}
+console.log(middlearr([8,5,6,10,3,9,4,2,1,0]))
 // 5. Realiza el ejercicio anterior de ejercicios para arreglos y objetos 5.1 y 5.2, pero usando funciones con parametros, y bucles.
 const usuarios = [
   {
